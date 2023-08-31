f7418b1756ae348aa19b983604e3fd4c99c71050
Alberto 'Smiker' Perez Area (Pinakure) <pinakure@users.noreply.github.com>
2023-08-31 04:05:21 +0200

    Repo cleanup
    - Remove trash files
    - Added progressbar plugin (wip)
    - Move progressbar files to progressbar plugin

--- RA/update.sh
+++
@@ -1,1 +0,0 @@
-wget https://retroachievements.org/user/smiker
---
+++ ramon/plugins/progressbar/plugin.py
@@ -0,0 +1,43 @@
+from classes.plugin import Plugin, px
+
+class plugin(Plugin):
+
+    def __init__(self):
+        Plugin.__init__(self)
+        #@ Plugin Settings
+        self.name        = 'progressbar'
+        self.description = 'RetroAchievements Current Game progress bar widget',
+        self.endpoint    = 'progress'
+        self.color       = '#ff0'
+
+        #@ Composer Settings
+        self.z_index     = 10
+        self.width       = 1024
+        self.height      = 128
+        self.scale       = 1   
+        self.y           = 800
+        #@ Vars
+
+
+
+
+
+        #@ Install settings
+        self.files      = [
+            'progress-overlay.png',
+        ]    
+
+    def composerVars(self):
+        return {
+            'width'   : px(self.width   ),
+            'height'  : px(self.height  ),
+            'left'    : px(self.x       ),
+            'top'     : px(self.y       ),
+            'scale'   : self.scale,
+            'z-index' : self.z_index,
+        }
+
+    def templateVars(self):
+        return {            
+        }
+
